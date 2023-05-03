b323edae64a4ac6c7cab90bdb23412fe3397a445
Anna <34191811+404unf@users.noreply.github.com>
2023-05-02 23:50:39 -0700

    Solved it

--- index.js
+++ index.js
@@ -1,1 +1,33 @@
 // Your code here
+
+const getDodger = document.getElementById('dodger');
+
+function moveDodgerLeft() {
+    const leftNumbers = getDodger.style.left.replace("px", "");
+    const left = parseInt(leftNumbers, 10);
+
+    if (left > 0) {
+        getDodger.style.left = `${left - 1}px`;
+    }
+}
+
+function moveDodgerRight() {
+    const leftNumbers = getDodger.style.left.replace("px", "");
+    const left = parseInt(leftNumbers, 10);
+
+    if (left > 0 && left < 360) {
+        getDodger.style.left = `${left + 1}px`;
+    }
+}
+
+document.addEventListener("keydown", function (e) {
+    if (e.key === "ArrowLeft") {
+        moveDodgerLeft();
+    }
+    if (e.key === "ArrowRight") {
+        moveDodgerRight();
+    }
+});
+
+
+
